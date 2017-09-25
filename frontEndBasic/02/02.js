693a9e486740080a0f15e1910d1a7d39748efed7
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2017-09-25 15:40:57 +0800

    02

--- frontEndBasic/02/02.js
+++ frontEndBasic/02/02.js
@@ -1,5 +1,35 @@
 $(function () {
 	"use strict";
 
-	$("")
+	$(document).ready(function () {
+		$(".green").addClass("current");
+		$("body").css("background-color", "#A3C5A8");
+		$(".nav").css("background-color", "green");
+	});
+
+	$(".red").on("click", function (e) {
+		removeCurrentClass();
+		$(".red").addClass("current");
+		$("body").css("background-color", "pink");
+		$(".nav").css("background-color", "red");
+	});
+
+	$(".green").on("click", function (e) {
+		removeCurrentClass();
+		$(".green").addClass("current");
+		$("body").css("background-color", "#A3C5A8");
+		$(".nav").css("background-color", "green");
+	});
+	$(".black").on("click", function (e) {
+		removeCurrentClass();
+		$(".black").addClass("current");
+		$("body").css("background-color", "gray");
+		$(".nav").css("background-color", "black");
+	});
+
+	var removeCurrentClass = function () {
+		$(".green").removeClass("current");
+		$(".red").removeClass("current");
+		$(".black").removeClass("current");
+	}
 });
