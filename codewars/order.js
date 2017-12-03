8864a08a97a580df9cd11f8ca10457cc19982c79
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2017-12-03 09:43:55 +0800

    finish order practice and get the best practice solution

--- codewars/order.js
+++ codewars/order.js
@@ -1,70 +1,43 @@
-(function(){
+(function () {
     "use strict";
 
-    function order(words){
-        // ...
-        if(words === "") {
+    function order(words) {
+        // sort the word according to the number of the word.
+        //  [inout] "is2 Thi1s T4est 3a"
+        //  [output] "Thi1s is2 3a T4est"
+
+        if (words === "") {
             return "";
         }
-
-        if(words.length == 1) {
+        if (words.length === 1) {
             return words;
         }
-        function getNum(word){
+
+        function getNum(word) {
             var num = "";
-            if(word) {
-                for (var i = 0; i < word.length; i++) {
-                    if (parseInt(word[i])) {
-                        num += word[i];
-                    }
+            word.split("").forEach(function (t) {
+                if (parseInt(t)) {
+                    num += t;
                 }
-                console.log(num);
-                return parseInt(num);
-            }
-            return;
-        };
+            });
+            return parseInt(num);
+        }
 
-        var wordList = words.split(" ");
-        console.log(wordList);
-        var quickSort = function (wordList) {
-            var pivotIndex = Math.floor(wordList.length / 2);
-            var pivotEle = getNum(wordList.splice(pivotIndex, 1)[0]);
-            var left = [];
-            var right = [];
-            for(var i = 0; i < wordList.length; i++) {
-                if(getNum(wordList[i] < pivotEle)) {
-                    left.push(wordList[i]);
-                } else {
-                    right.push(wordList[i]);
-                }
-            }
-            return quickSort(left).concat([pivotEle], quickSort(right));
-        };
+        var wordList = words.split(" ").sort(function (a, b) {
+            return getNum(a) > getNum(b) ? 1 : -1;
+        });
 
-        return quickSort(wordList);
-        //
-        // var numList = [];
-        // var index = 0;
-        // wordList.forEach(function (w) {
-        //     numList[index] = getNum(w);
-        //     index++;
-        // });
-        //
-        //
-        //
-        // var len = wordList.length;
-        // for(var i = 0; i < len; i++) {
-        //     for(var j = i+1; j < len-1; j++) {
-        //         console.log(wordList[i] + " *** " + wordList[j]);
-        //         if(getNum(wordList[j] > getNum(wordList[j+1]))) {
-        //             var temp = wordList[j];
-        //             wordList[i] = wordList[j+1];
-        //             wordList[j+1] = temp;
-        //         }
-        //     }
-        // }
-        // return wordList.join(" ");
+        return wordList.join(" ");
     }
+
     console.log(order("is2 Thi1s T4est 3a"));
+
+    // best practice in codewars of this Kata
+    var bestPractice = function (words) {
+
+        return words.split(" ").sort(function (a, b) {
+            return a.match(/\d/) - b.match(/\d/); // here is really clever that I never thought.
+        }).join(" ");
+    };
 
 })();
