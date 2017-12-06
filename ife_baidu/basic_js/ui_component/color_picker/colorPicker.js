27b1a8382004f52799f836c86fe784774199d214
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2017-12-07 00:26:25 +0800

    add color picker html

---
+++ ife_baidu/basic_js/ui_component/color_picker/colorPicker.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Selector = function () {
+
+};
+
+var init = (function () {
+    new ClipboardData("#ws-rgb-btn");
+    new ClipboardData("#ws-hsl-btn");
+    new ClipboardData("#ws-hsv-btn");
+})();
