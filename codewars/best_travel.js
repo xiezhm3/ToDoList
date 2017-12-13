806940f0b2c708007c0e0b20c3bdb427579a7aa5
xiezhm3 <xiezhm3@mail2.sysu.edu.cn>
2017-12-13 23:13:56 +0800

    add best travel

---
+++ codewars/best_travel.js
@@ -0,0 +1,16 @@
+(function () {
+    "use strict";
+
+
+    var chooseBestSum = function (t, k, ls) {
+
+        var min = null;
+
+        for(var i = 0; i < ls.length; i++) {
+
+        }
+
+    };
+
+
+})();
